e9cfe3d071f7628325f5527bf7a46978540fb0a8
MaciejZboch <maciejzboch@gmail.com>
2024-07-24 20:24:04 +0200

    feat: refactor tea routes

--- routes/tea.js
+++ routes/tea.js
@@ -11,76 +11,26 @@ const {storage} = require('../cloudinary');
 const vendor = require('../models/vendor');
 const upload = multer({ storage});
 const currentYear = new Date().getFullYear();
-const { cloudinary } = require('../cloudinary')
+const { cloudinary } = require('../cloudinary');
+const tea = require('../controllers/tea');
 
-
-router.get('/', (req, res) => {
-    res.render('teas/index')
-})
 router.get('/browse', (req, res) => {
     res.render('teas/browse', {Tea, DataTable})
 
 })
+router.route('/')
+.get(tea.index)
+.post(isLoggedIn, upload.array('image'), validateTea, catchAsync(tea.new))
 
 
-router.get('/new', isLoggedIn, catchAsync(async (req, res) => {
-    const vendors = await Vendor.find();
-    const producers = await Producer.find();
-    res.render('teas/new', {currentYear, vendors, producers})
-}))
-router.post('/', isLoggedIn, upload.array('image'), validateTea, async (req, res) => {
-    const newTea = new Tea(req.body.tea);
-    newTea.author = req.user._id;
-    newTea.vendor = await Vendor.findOne({ name: req.body.vendor.name});
-    newTea.producer = await Producer.findOne({ name: req.body.producer.name});
- newTea.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
- await newTea.save();
- res.redirect('/tea/' + newTea._id);
-})
+router.get('/new', isLoggedIn, catchAsync(tea.newForm))
 
+router.route('/:id')
+.get(catchAsync(tea.show))
+.put(isLoggedIn, isAuthor, upload.array('image'), validateTea, catchAsync(tea.update))
+.delete(isLoggedIn, isAuthor, catchAsync(tea.delete))
 
-router.get('/:id', catchAsync(async (req, res) => {
-    const tea = await Tea.findById(req.params.id).populate({
-        path: 'reviews',
-        populate: {
-            path: 'author'
-        }
-    }).populate('producer').populate('vendor');
-    if (!tea) {
-        req.flash('error', 'Cannot find that tea!')
-        return res.redirect('/tea')}
-res.render('teas/show', {tea})
-    }))
+router.get('/:id/edit', isLoggedIn, isAuthor, validateTea, catchAsync(tea.editForm))
 
-router.get('/:id/edit', isLoggedIn, isAuthor, validateTea, catchAsync(async (req, res) => {
-    const t = await Tea.findById(req.params.id).populate('vendor').populate('producer');
-    if (!t) {
-        req.flash('error', 'Cannot find that tea!')
-        return res.redirect('/tea')}
-        const vendors = await Vendor.find();
-        const producers = await Producer.find();    
-    res.render('teas/edit', {t, currentYear, vendors, producers})
-}))
-
-router.put('/:id', isLoggedIn, isAuthor, upload.array('image'), validateTea, catchAsync(async (req, res) => {
-    const foundTea = await Tea.findByIdAndUpdate(req.params.id, {...req.body.tea});
-    if (req.files) {
-        const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
-    await foundTea.save();
-    if (req.body.deleteImages) {
-        for (let filename of req.body.deleteImages) {
-            await cloudinary.uploader.destroy(filename);
-        }
-        foundTea.images.push(...imgs)}
-        await foundTea.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } })
-    }
-    req.flash('success', 'Succesfully updated!');
-    res.redirect(`/tea/${foundTea._id}`);
-}))
-
-    router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
-        await Tea.findByIdAndDelete(req.params.id);
-        res.redirect('/tea');
-    }))
 
 module.exports = router;
