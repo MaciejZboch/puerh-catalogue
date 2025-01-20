8eab3abe65653d993c3756cbcad30eb9652d5e0b
MaciejZboch <maciejzboch@gmail.com>
2025-01-20 16:28:27 +0100

    feat: add user access to model

--- models/user.js
+++ models/user.js
@@ -2,11 +2,20 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require("passport-local-mongoose");
 
+/*const UserAccess = {
+  MOD: "mod",
+  USER: "user",
+  ADMIN: "admin",
+};*/
+
 const UserSchema = new Schema({
   email: {
     type: String,
     required: true,
     unique: true,
+  },
+  moderator: {
+    type: Boolean,
   },
 });
 UserSchema.plugin(passportLocalMongoose);
