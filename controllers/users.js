00bb287581d380541509c1bb1d81a028ffb9d408
MaciejZboch <maciejzboch@gmail.com>
2025-05-16 20:01:13 +0200

    fix: issue with registration without avatar

--- controllers/users.js
+++ controllers/users.js
@@ -27,8 +27,10 @@ module.exports.register = async (req, res) => {
     try {
       const user = new User({ email, username });
       user.moderator = false;
-      user.image = req.file;
-      user.image = { url: req.file.path, filename: req.file.filename };
+      if (req.file) {
+        user.image = req.file;
+        user.image = { url: req.file.path, filename: req.file.filename };
+      }
       const registeredUser = await User.register(user, password);
       req.login(registeredUser, (err) => {
         if (err) return next(err);
